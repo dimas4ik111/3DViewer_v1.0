e51241b10ea11b9af4ef3044fdf211a31c382994
Hubert Furr <hubertfu@student.21-school.ru>
2022-09-13 22:46:41 +0300

    parser tmp version

--- src/backend/parser.c
+++ src/backend/parser.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
 void parse_file(char *file_path) {
-  FILE *file = fopen(file_path, "r");
-  printf("[%s]\n", file_path);
-  fclose(file);
+  FILE *file;
+  char buff[1024];
+  file = fopen(file_path, "r");
+  if (file != NULL) {
+    printf("[%s]\n\n", file_path);
+    while (!feof(file)) {
+      fgets(buff, 1024, file);
+      if (buff[0] == 'v' && buff[1] == ' ') {
+        printf("%s", buff);
+      } else if (buff[0] == 'f' && buff[1] == ' ') {
+        printf("%s", buff);
+      }
+      
+    }
+
+    fclose(file);
+  }
 }
--- src/check/test_calc.c
+++ src/check/test_calc.c
@@ -7,14 +7,14 @@
  * Тесты на некорректные данные
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  */
 
-START_TEST(test_calc_fail1) {}
+START_TEST(test_parser_fail1) {}
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * Тесты на корректные данные
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/
 
-START_TEST(test_calc_ok1) {
-  char *obj_path = "./objfiles/cube.obj";
+START_TEST(test_parser_ok1) {
+  char *obj_path = "check/objfiles/cube.obj";
   parse_file(obj_path);
 }
 
@@ -25,9 +25,9 @@ Suite *parse_file_suite(void) {
   s = suite_create("parse_file");
   tc_core = tcase_create("Core");
 
-  tcase_add_test(tc_core, test_calc_fail1);
+  tcase_add_test(tc_core, test_parser_fail1);
 
-  tcase_add_test(tc_core, test_calc_ok1);
+  tcase_add_test(tc_core, test_parser_ok1);
 
   suite_add_tcase(s, tc_core);
   return s;
