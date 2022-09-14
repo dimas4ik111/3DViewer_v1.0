ae9b7722ca0677024517064f6cc593d2300c27be
Hubert Furr <hubertfu@student.21-school.ru>
2022-09-14 15:45:19 +0300

    obj parser: slow version 0.1

--- src/backend/parser.c
+++ src/backend/parser.c
@@ -1,21 +1,125 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void parse_file(char *file_path) {
+#include "backend.h"
+
+void s21_parse_file(char *file_path, s21_obj_data *data) {
   FILE *file;
   char buff[1024];
   file = fopen(file_path, "r");
+  // TODO(hubertfu): file open error
   if (file != NULL) {
-    printf("[%s]\n\n", file_path);
+    data->num_of_f = 0;
+    data->num_of_v = 0;
+
     while (!feof(file)) {
       fgets(buff, 1024, file);
       if (buff[0] == 'v' && buff[1] == ' ') {
-        printf("%s", buff);
+        s21_parse_line_v(buff, data);
       } else if (buff[0] == 'f' && buff[1] == ' ') {
-        printf("%s", buff);
+        s21_parse_line_f(buff, data);
       }
-      
+      // if (data->num_of_v % 1000 == 0) {
+      //   printf("{%u}\n", data->num_of_v);
+      // }
     }
 
     fclose(file);
+    // s21_print_obj_data(data);
+  }
+}
+
+void s21_parse_line_v(char *line, s21_obj_data *data) {
+  // TODO(hubertfu): handle incoorect lines
+  if (data->num_of_v == 0) {
+    // TODO(hubertfu): memory allocation by blocks
+    data->array_of_v = malloc(1 * sizeof(s21_vertex));
+  } else {
+    data->array_of_v =
+        realloc(data->array_of_v, (data->num_of_v + 1) * sizeof(s21_vertex));
+  }
+
+  if (data->array_of_v == NULL) {
+    // TODO(hubertfu): handle memory errors
+  }
+
+  // TODO(hubertfu): scanf memory errors
+  s21_vertex tmp = {0, 0, 0};
+  sscanf(line, "v %f %f %f", &tmp.x, &tmp.y, &tmp.z);
+  data->array_of_v[data->num_of_v] = tmp;
+  ++(data->num_of_v);
+}
+
+void s21_parse_line_f(char *line, s21_obj_data *data) {
+  // TODO(hubertfu): handle incoorect lines
+  char *ptr = line;
+  int flag = 0;
+
+  if (data->num_of_f == 0) {
+    // TODO(hubertfu): memory allocation by blocks
+    // TODO(hubertfu): handle memory errors
+    data->array_of_f = malloc(1 * sizeof(s21_vertex));
+  } else {
+    data->array_of_f =
+        realloc(data->array_of_f, (data->num_of_f + 1) * sizeof(s21_polygon));
+  }
+
+  data->array_of_f[data->num_of_f].num_of_points = 0;
+
+  while (*ptr != '\0') {
+    if (s21_is_number(*ptr) && flag == 0) {
+      unsigned int tmp;
+      sscanf(ptr, "%u", &tmp);
+
+      flag = 1;
+      if (data->array_of_f[data->num_of_f].num_of_points == 0) {
+        // TODO(hubertfu): memory allocation by blocks
+        // TODO(hubertfu): handle memory errors
+        data->array_of_f[data->num_of_f].points =
+            malloc(1 * sizeof(unsigned int));
+      } else {
+        data->array_of_f[data->num_of_f].points =
+            realloc(data->array_of_f[data->num_of_f].points,
+                    (data->array_of_f[data->num_of_f].num_of_points + 1) *
+                        sizeof(unsigned int));
+      }
+      data->array_of_f[data->num_of_f]
+          .points[data->array_of_f[data->num_of_f].num_of_points] = tmp;
+      ++(data->array_of_f[data->num_of_f].num_of_points);
+    }
+    if (*ptr == ' ') {
+      flag = 0;
+    }
+    ++ptr;
+  }
+
+  ++(data->num_of_f);
+}
+
+s21_boolean s21_is_number(char symbol) {
+  return ((int)symbol >= 48 && (int)symbol <= 57);
+}
+
+void s21_destroy_obj_data(s21_obj_data *data) {
+  free(data->array_of_v);
+  for (unsigned int i = 0; i < data->num_of_f; i++) {
+    free(data->array_of_f[i].points);
+  }
+  free(data->array_of_f);
+}
+
+void s21_print_obj_data(s21_obj_data *data) {
+  printf("\n\n");
+  for (unsigned int i = 0; i < data->num_of_v; i++) {
+    printf("x:%10f y:%10f z:%10f\n", data->array_of_v[i].x,
+           data->array_of_v[i].y, data->array_of_v[i].z);
+  }
+
+  for (unsigned int i = 0; i < data->num_of_f; i++) {
+    printf("f: ");
+    for (unsigned int j = 0; j < data->array_of_f[i].num_of_points; j++) {
+      printf("%u ", data->array_of_f[i].points[j]);
+    }
+    printf("\n");
   }
 }
--- src/check/test_parse.c
+++ src/check/test_parse.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "./test.h"
 
@@ -15,7 +16,22 @@ START_TEST(test_parser_fail1) {}
 
 START_TEST(test_parser_ok1) {
   char *obj_path = "check/objfiles/cube.obj";
-  parse_file(obj_path);
+  test_parser(obj_path);
+}
+
+START_TEST(test_parser_ok2) {
+  char *obj_path = "check/objfiles/Gun.obj";
+  test_parser(obj_path);
+}
+
+START_TEST(test_parser_ok3) {
+  // char *obj_path = "check/objfiles/Kalashnikov.obj";
+  // test_parser(obj_path);
+}
+
+START_TEST(test_parser_ok4) {
+  // char *obj_path = "check/objfiles/Bugatti.obj";
+  // test_parser(obj_path);
 }
 
 Suite *parse_file_suite(void) {
@@ -28,7 +44,22 @@ Suite *parse_file_suite(void) {
   tcase_add_test(tc_core, test_parser_fail1);
 
   tcase_add_test(tc_core, test_parser_ok1);
+  tcase_add_test(tc_core, test_parser_ok2);
+  tcase_add_test(tc_core, test_parser_ok3);
+  tcase_add_test(tc_core, test_parser_ok4);
 
   suite_add_tcase(s, tc_core);
   return s;
+}
+
+void test_parser(char *obj_path) {
+  s21_obj_data data;
+  clock_t start, end;
+  start = clock();
+  s21_parse_file(obj_path, &data);
+  s21_destroy_obj_data(&data);
+  end = clock();
+  printf("v: %u\n", data.num_of_v);
+  printf("f: %u\n", data.num_of_f);
+  printf("Parsing time: %.7f\n", (double)(end - start) / (CLOCKS_PER_SEC));
 }
