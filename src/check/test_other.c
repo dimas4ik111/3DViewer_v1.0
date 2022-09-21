976c8ad2619c30ebb58ff63417f419c849c149ff
Hubert Furr <hubertfu@student.21-school.ru>
2022-09-21 16:43:47 +0300

    final tests

--- src/check/test_other.c
+++ src/check/test_other.c
@@ -5,7 +5,7 @@
 #include "./test.h"
 
 START_TEST(test_other_ok1) {
-  char *obj_path = "check/objfiles/cube.obj";
+  char *obj_path = "_objfiles/_cube.obj";
   s21_obj_data data;
   s21_obj_data data_copy;
   test_other(obj_path, &data, S21_FALSE);
